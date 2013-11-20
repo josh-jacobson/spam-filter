cf2ea35566095577c6d5b8afc182c42b381e6f2f
Josh Jacobson <joshjacobson14@gmail.com>
2013-11-19 19:17:50 -0600

    function outlines

---
+++ makedictionary.m
@@ -0,0 +1,7 @@
+function makedictionary( spam_directory, ham_directory, dictionary_filename) 
+%UNTITLED Summary of this function goes here
+%   Detailed explanation goes here
+
+
+end
+
---
+++ spamsort.m
@@ -0,0 +1,8 @@
+function spamsort(mail_directory, spam_directory, ham_directory, dictionary_filename, spam_prior_probability) 
+
+%UNTITLED2 Summary of this function goes here
+%   Detailed explanation goes here
+
+
+end
+

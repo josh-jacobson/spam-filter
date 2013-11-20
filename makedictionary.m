c9f3e393312eabf93b45111865739a339d1a5ae5
Josh Jacobson <joshjacobson14@gmail.com>
2013-11-19 19:59:31 -0600

    Basic file reading for makedictionary.m

--- makedictionary.m
+++ makedictionary.m
@@ -1,4 +1,4 @@
-function makedictionary( spam_directory, ham_directory, dictionary_filename) 
+function [spam, ham] = makedictionary( spam_directory, ham_directory, dictionary_filename) 
 %MAKEDICTIONARY constructs a dictionary file for use in spam filtering.
 %   The inputs spam_directory and ham_directory are the filepaths to two 
 %   folders, each of which contains one or more ASCII text files. This
@@ -6,6 +6,34 @@ function makedictionary( spam_directory, ham_directory, dictionary_filename)
 %   directory, with filename dictionary_filename. Each line of the 
 %   dictionary file has the following format: 
 %   [word] [P(word|spam)] [P(word|ham)]
+
+    spam_files = dir('spam');
+    spam_filenames = {spam_files.name};
+    spamCount = length(spam_filenames);
+    spam = cell(1,spamCount);
+    
+    ham_files = dir('ham');
+    ham_filenames = {ham_files.name};
+    hamCount = length(ham_filenames);
+    ham = cell(1,spamCount);
+    
+    
+    cd('spam');
+    for i = 4:spamCount % Need to remedy this with a regex to handle ., .., ds_store
+        id = fopen(spam_filenames{i});
+        text = textscan(id, '%s');
+        spam{i} = text{1};
+        fclose(id);
+    end
+    
+    cd('../ham');
+    for i = 4:hamCount % Need to remedy this with a regex to handle ., .., ds_store
+        id = fopen(ham_filenames{i});
+        text = textscan(id, '%s');
+        ham{i} = text{1};
+        fclose(id);
+    end
+    cd('../');
 
 
 end

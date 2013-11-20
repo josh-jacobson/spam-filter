f37be1360e2a1accd1b86fe54841aca8ad5a319a
Josh Jacobson <joshjacobson14@gmail.com>
2013-11-19 19:31:18 -0600

    Spam and ham data

--- makedictionary.m
+++ makedictionary.m
@@ -2,9 +2,10 @@ function makedictionary( spam_directory, ham_directory, dictionary_filename)
 %MAKEDICTIONARY constructs a dictionary file for use in spam filtering.
 %   The inputs spam_directory and ham_directory are the filepaths to two 
 %   folders, each of which contains one or more ASCII text files. This
-%   function constructs a space delimited dictionary file at the path 
-%   specified by dictionary_filename. Each line of the dictionary file
-%   has the following format: [word] [P(word|spam)] [P(word|ham)]
+%   function constructs a space delimited dictionary file in the current 
+%   directory, with filename dictionary_filename. Each line of the 
+%   dictionary file has the following format: 
+%   [word] [P(word|spam)] [P(word|ham)]
 
 
 end

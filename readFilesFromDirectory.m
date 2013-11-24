6e818aced6ceba7e0512c0003a00fdce57361d7b
Josh Jacobson <joshjacobson14@gmail.com>
2013-11-24 13:14:15 -0600

    Generalize file addressing for reading from any dictory

--- readFilesFromDirectory.m
+++ readFilesFromDirectory.m
@@ -12,22 +12,22 @@ function [finalTextArray, wordList, finalFilenames] = readFilesFromDirectory(dir
     fileCount = length(filenames);
     textArray = cell(1,fileCount);
     
-    cd(directory);
     for i = 1:fileCount
-        filename = filenames{i};
+        filename = filenames{i}
+        filepath = strcat(directory,'/',filename)
         if filename(1) == '.' || strcmpi(filename, 'cmds') % ignore ., .., .DS_STORE and cmds
             numFilesToIgnore = numFilesToIgnore + 1;
             filesToIgnore(numFilesToIgnore) = i;
         else
             % Find the end of the header
-            id = fopen(filename);
+            id = fopen(filepath);
             text = textscan(id, '%s', 'Delimiter', '\n');
             textLines = text{1};
             fclose(id);
             [~,dividingLine] = ismember('', textLines);
             
             % Read in the input file word by word, skipping the header
-            id = fopen(filename);
+            id = fopen(filepath);
             text = textscan(id, '%s', 'Delimiter', ' ,.;!?{}[]"#-=+\n', 'MultipleDelimsAsOne',1, 'HeaderLines', dividingLine);
             text = text{1};
             fclose(id);
@@ -50,7 +50,6 @@ function [finalTextArray, wordList, finalFilenames] = readFilesFromDirectory(dir
             wordList = [wordList;text]; % Append this file's words to the word list
         end
     end
-    cd('../');
     
     % Remove empty elements from the array (from ignored files)
     finalTextArray = cell(1, fileCount - numFilesToIgnore);
